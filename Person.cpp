4d2dabab073d1f4c1d7b18b959be2fd7c2c2bdaa
ktw1404 <59117121+ktw1404@users.noreply.github.com>
2019-12-22 01:02:18 +0900

    Add files via upload

---
+++ Person.cpp
@@ -0,0 +1,52 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+
+class Person {
+	string ID; //각 사람의 ID를 string type으로 저장
+	string password; //각 사람의 ID를 string type으로 저장
+	int type;
+public:
+	Person() {
+		this -> ID = "";
+		this-> password = "";
+		this-> type = 0;
+	};
+	void set_ID(string id) 
+	{
+		this -> ID = id;
+		cout << "당신의 ID는 " << id << "입니다."; 
+	}; //string을 입력받아 ID를 string type으로 저장
+
+	string get_ID(void) 
+	{
+		cout << "당신의 ID는 " << ID << "입니다.";
+		return ID;
+	}; //ID를 string type으로 return
+	
+	void set_password(string pw) 
+	{
+		this-> password = pw;
+		cout << "당신의 PW는 " << pw << "입니다.";
+	}; //string을 입력받아 password를 string type으로 저장
+
+
+	string get_password(void) 
+	{
+		return password;
+	}; //password를 string type으로 return
+	
+	void set_type(int input) 
+	{
+		this->type = input;
+	}; //string을 입력받아 type를 string type으로 저장
+
+	int get_type(void) 
+	{
+		return type;
+	}; //type를 string type으로 return
+};
