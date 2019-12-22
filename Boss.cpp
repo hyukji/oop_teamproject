714428099ea38f3e666852b0edf0070d36918c8d
ktw1404 <59117121+ktw1404@users.noreply.github.com>
2019-12-22 20:43:13 +0900

    Update Boss.cpp

--- Boss.cpp
+++ Boss.cpp
@@ -25,26 +25,4 @@ public:
 		cin >> in;
 		this->store_PH = in;
 	}; //가게 전화번호를 cin으로 string type으로 받아서 store_PH에 넣어준다.
-
-	void store_open(void) {
-		if (store_open == false)
-			this -> store_open = true;
-		bool poss = checking();
-		if (poss == true) {
-			this->store_open = false;
-		}
-		else {
-			cout << "조리가 완료되지 않은 항목이 있어 가게문을 닫을 수 없습니다." << endl;
-		}
-	}; //open이 false면 true로, true면 false로 바꿔준다.-true에서 false로 갈 때는 cooking_over을 실해해서 true일때만 닫을 수 있다. 
-	bool checking(void) {
-		for (bool order_stat : order_list)
-			if (order_stat == false) {
-				cout << "완료되지 않은 조리가 존재합니다." << endl;
-				break;
-			}
-		cout << "모두 조리가 완료된 상태입니다." << endl;
-		return true;
-	}; //order_list의 주문완료 여부를 확인하여 모두 true일때만 true를 return, 하나라도 주문 남아있으면 false return
-	//지금 order_list의 원소중 몇번째 항목이 조리완료인지 몰라 일단은 저렇게 표현해뒀습니다. 담당하신분 
 };
